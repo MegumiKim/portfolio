2b2a60435a033e5911aaf47fe6bbebbedfed7ce4
Megumi Kimura <kimuramegumi@gmail.com>
2022-10-05 21:13:03 +0200

    fix img path

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
-  console.log("click");
   if (!menuOpen) {
     menuBtn.classList.add("open");
     menuOpen = true;
+    nav.style.display = "flex";
   } else {
     menuBtn.classList.remove("open");
     menuOpen = false;
+    nav.style.display = "none";
   }
 });
