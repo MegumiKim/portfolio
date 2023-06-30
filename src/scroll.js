598ca224f5b85b4053489f9c5e6a9cfa37bee7a5
Megumi Kimura <kimuramegumi@gmail.com>
2023-06-30 15:43:07 +0200

    update cipher screenshot

--- src/scroll.js
+++ src/scroll.js
@@ -1,23 +1,23 @@
-// const scrollElements = document.querySelectorAll(".scroll");
+const scrollElements = document.querySelectorAll(".scroll");
 
-// scrollElements.forEach((element) => {
-//   element.style.opacity = 0;
-// });
+scrollElements.forEach((element) => {
+  element.style.opacity = 0;
+});
 
-// function isElementInView(element) {
-//   const elementTop = element.getBoundingClientRect().top;
-//   return (
-//     elementTop <= (window.innerHeight || document.documentElement.clientHeight)
-//   );
-// }
-// const displayScrollElement = (element) => {
-//   element.style.opacity = 1;
-// };
+function isElementInView(element) {
+  const elementTop = element.getBoundingClientRect().top;
+  return (
+    elementTop <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+const displayScrollElement = (element) => {
+  element.style.opacity = 1;
+};
 
-// export function handleScroll() {
-//   scrollElements.forEach((element) => {
-//     if (isElementInView(element, 100)) {
-//       displayScrollElement(element);
-//     }
-//   });
-// }
+export function handleScroll() {
+  scrollElements.forEach((element) => {
+    if (isElementInView(element, 100)) {
+      displayScrollElement(element);
+    }
+  });
+}
