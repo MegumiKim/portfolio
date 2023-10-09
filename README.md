a8016f6a713a10e9555025ff6190aba9f31028ad
Megumi Kimura <kimuramegumi@gmail.com>
2023-10-09 15:39:57 +0200

    clean up

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Megumi's Portfolio
 
-[demo site](https://megumikim.github.io/portfolio/)
+[demo site](https://portfolio.megumi.no)
 
 ## Brief
 
 Welcome to My portfolio.
 Please checkout my recent dev projects.
+
+## Built With
+
+- HTML
+- CSS
+- JS
+
+## Contact
+
+Any issues / feedback, reach me on kimuramegumi@gmail.com
--- src/script.js
+++ src/script.js
@@ -1,36 +1,15 @@
-// import { handleScroll } from "./scroll.js";
-// import { setScrollVar, shrinkHeader } from "./scrollHandler.js";
 import projects from "./projects.js";
 import card from "./projectCard.js";
 import { toggleMenu } from "./menuBtn.js";
 import { scrollObserver } from "./scrollObervers.js";
 
 toggleMenu();
+
 //Creates Projects
 projects.forEach((project) => {
   const projectCard = card(project);
 
   document.querySelector(".projects-wrapper").innerHTML += projectCard;
 });
-
-// const projectThumbs = document.querySelectorAll(".img-thumbnail");
-
-// projectThumbs.forEach((thumb, i) => {
-//   if (i % 2 === 0) {
-//     thumb.classList.add("from-left");
-//   } else {
-//     thumb.classList.add("from-right");
-//   }
-// });
-
-// const textWrappers = document.querySelectorAll(".card-text-wrapper");
-
-// textWrappers.forEach((wrapper, i) => {
-//   if (i % 2 === 0) {
-//     wrapper.classList.add("from-right");
-//   } else {
-//     wrapper.classList.add("from-left");
-//   }
-// });
 
 scrollObserver();
