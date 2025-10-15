544c04ccbe9530400b08a869c11952f66372d689
Megumi Kimura <kimuramegumi@gmail.com>
2025-10-15 22:02:59 +0200

    update projects

--- src/projects.js
+++ src/projects.js
@@ -6,8 +6,28 @@ const projects = [
     url: "https://beta.futureslab.dev/",
     repo: "https://github.com/MegumiKim/FuturesLab",
     description:
-      "Schibsted Futures Lab's official website and professional documentation tool. Fully customised CMS using Sanity.",
+      "Schibsted Futures Lab's official website and professional documentation tool. Fully customized CMS using Sanity.",
     builtWith: "Sveltekit • TypeScript • Sanity • Sass",
+  },
+  {
+    category: "Website / CMS",
+    name: "Fotterapi Langholt",
+    image: "/img/fotterapi.png",
+    url: "https://fotterapi-langholt.vercel.app/",
+    repo: "https://github.com/MegumiKim/Fotterapi_Langholt",
+    description:
+      "Marketing website for a small scale business. Customized Sanity enables the owner to update the contents. ",
+    builtWith: "Sveltekit • TypeScript • Sanity CMS • Sass",
+  },
+  {
+    category: "SPA",
+    name: "Cipher",
+    image: "/img/cipher.jpg",
+    url: "https://cipher-encryption.vercel.app/",
+    repo: "https://github.com/MegumiKim/cipher.git",
+    description:
+      "A personal vanilla JS project, focused on a minimalistic user interface. The message encryption application allows users to encrypt and decrypt message on one page.",
+    builtWith: "HTML • CSS • JS",
   },
   {
     category: "Open Source Project",
@@ -19,27 +39,6 @@ const projects = [
       "An open-source project of developing a dynamic learning platform with a range of features, including bookmarking, search capabilities, text-to-speech functionality, and an AI-powered chatbox. My primary contributions were in enhancing the chatbox's frontend, fixing bugs, and improving the overall user experience.",
     builtWith:
       "Sveltekit • TypeScript • Tailwind • Skeleton UI • Vite • Open AI",
-  },
-  {
-    category: "Web App",
-    name: "Čika Petar's",
-    image: "/img/barThumb.jpg",
-    url: "https://bar.megumi.no",
-    repo: "https://github.com/MegumiKim/bar.git",
-    description:
-      "Personal Frontend project of cocktail receipe web app. Users can explore cocktail recipes, look up ingredients, and save their favorites. The primary objective of this project was to enhance my skills in using Sveltekit and TypeScript while creating a user-friendly cocktail resource.",
-    builtWith: "Sveltekit • TypeScript • Tailwind • Skeleton UI • Vite",
-  },
-  {
-    category: "Web App",
-    name: "Book Journal",
-    image: "/img/bookjournal.jpg",
-    url: "https://bookjournal.megumi.no",
-    repo: "https://github.com/MegumiKim/book_share",
-    description:
-      "Full stack personal project with PERN Stack powered by Google Books API. The web application allows users to create accounts, explore books, maintain reading list and post reviews and get recommendations. The project was in depth practice of both frontend and backend tools, including database integration, API creation, and cloud deployment on AWS.",
-    builtWith:
-      "Postgres • Express • React • Node.js • DaisyUI • TypeScript • AWS",
   },
   {
     category: "Website",
@@ -55,32 +54,44 @@ const projects = [
     category: "Web App",
     name: "Max Value",
     image: "/img/MaxValue.jpg",
-    url: "https://max-value.megumi.no",
+    url: "https://max-value-alpha.vercel.app/",
     repo: "https://github.com/MegumiKim/maxvalue.git",
     description:
       "An interface for an fictional online shopping platform, utilizing data from a Dummy API. The application enables users to browse products, perform searches, and simulate a realistic purchasing process. The primary focus of this project was to acquire practical expertise in building web applications with React.",
     builtWith: "React • TailwindCSS",
   },
-  {
-    category: "SPA",
-    name: "Cipher",
-    image: "/img/cipher.jpg",
-    url: "https://cipher.megumi.no",
-    repo: "https://github.com/MegumiKim/cipher.git",
-    description:
-      "A personal vanilla JS project, focused on a minimalistic user interface. The message encryption application allows users to encrypt and decrypt message on one page.",
-    builtWith: "HTML • CSS • JS",
-  },
-  {
-    category: "auction site",
-    name: "bid",
-    image: "/img/bid_screen.png",
-    url: "https://bid.megumi.no",
-    repo: "https://github.com/MegumiKim/bid.git",
-    description:
-      "Vanilla JavaScript frontend project featuring an interactive auction platform where users can create their own listings and bid on listings created by others with fictional currency.",
-    builtWith: "JS • Bootstrap • Saas",
-  },
+  // {
+  //   category: "Web App",
+  //   name: "Čika Petar's",
+  //   image: "/img/barThumb.jpg",
+  //   url: "https://bar.megumi.no",
+  //   repo: "https://github.com/MegumiKim/bar.git",
+  //   description:
+  //     "Personal Frontend project of cocktail receipe web app. Users can explore cocktail recipes, look up ingredients, and save their favorites. The primary objective of this project was to enhance my skills in using Sveltekit and TypeScript while creating a user-friendly cocktail resource.",
+  //   builtWith: "Sveltekit • TypeScript • Tailwind • Skeleton UI • Vite",
+  // },
+  // {
+  //   category: "Web App",
+  //   name: "Book Journal",
+  //   image: "/img/bookjournal.jpg",
+  //   url: "https://bookjournal.megumi.no",
+  //   repo: "https://github.com/MegumiKim/book_share",
+  //   description:
+  //     "Full stack personal project with PERN Stack powered by Google Books API. The web application allows users to create accounts, explore books, maintain reading list and post reviews and get recommendations. The project was in depth practice of both frontend and backend tools, including database integration, API creation, and cloud deployment on AWS.",
+  //   builtWith:
+  //     "Postgres • Express • React • Node.js • DaisyUI • TypeScript • AWS",
+  // },
+
+  // {
+  //   category: "auction site",
+  //   name: "bid",
+  //   image: "/img/bid_screen.png",
+  //   url: "https://bid.megumi.no",
+  //   repo: "https://github.com/MegumiKim/bid.git",
+  //   description:
+  //     "Vanilla JavaScript frontend project featuring an interactive auction platform where users can create their own listings and bid on listings created by others with fictional currency.",
+  //   builtWith: "JS • Bootstrap • Saas",
+  // },
 
   // {
   //   category: "Social Media site",
@@ -92,7 +103,6 @@ const projects = [
   //     "An interface for an fictional online shopping platform, utilizing data from a Noroff API.",
   //   builtWith: "React • Styled Components",
   // },
-
   ,
 ];
 
